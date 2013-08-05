37a2d058ede3bcb4f202cb421f4e109255239ce5
Alberto Gomez <alb.gomez@gmail.com>
2013-08-05 21:48:13 +0200

    Código Inicial

---
+++ WFTest.Bussines/Persons.Bussines.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BranchAbstraction.Data;
+using System.Data.Linq;
+
+namespace BranchAbstraction.Bussines
+{
+    public class PersonsBussines
+    {
+        public List<Person> GetPersons()
+        {
+            using (AdventureWorks2012Context aworkContext = new AdventureWorks2012Context())
+            {
+                return aworkContext.People.ToList();
+            }
+        }
+        public List<Person> GetPersonByPersonType(string personType)
+        {
+            return GetPersons().Where(person => person.PersonType == personType).ToList(); 
+        }
+    }
+}
---
+++ WFTest/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BranchAbstraction.Bussines;
+using BranchAbstraction.Data;
+
+namespace BranchAbstraction.Presentation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BotonCargaHombres_Click(object sender, EventArgs e)
+        {
+            List<Person> personsEM = new PersonsBussines().GetPersonByPersonType("EM");
+            dataGridView1.DataSource = personsEM;           
+
+        }
+    }
+}

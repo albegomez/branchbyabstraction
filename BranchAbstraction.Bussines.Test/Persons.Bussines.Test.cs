bcdd1577144570b1544ff305f52472d86faa6b36
Alberto Gomez <alb.gomez@gmail.com>
2013-08-05 22:28:45 +0200

    Creando cobertura de test

---
+++ BranchAbstraction.Bussines.Test/Persons.Bussines.Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BranchAbstraction.Data;
+using System.Collections.Generic;
+
+namespace BranchAbstraction.Bussines.Test
+{
+    [TestClass]
+    public class Persons
+    {
+        [TestMethod]
+        public void ObtenerTodoslasPersonas()
+        {
+            List<Person> personas = new PersonsBussines().GetPersons();
+            Assert.AreEqual("Ken", personas[0].FirstName);
+
+        }
+        [TestMethod]
+        public void ObtenerTodoslasPersonasVarones()
+        {
+            List<Person> personas = new PersonsBussines().GetPersonByPersonType("EM");
+            Assert.AreEqual("Ken", personas[0].FirstName);
+
+        }
+
+    }
+}
--- WFTest.Bussines/Persons.Bussines.cs
+++ WFTest.Bussines/Persons.Bussines.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BranchAbstraction.Data;
+using System.Linq;
 using System.Data.Linq;
 
 namespace BranchAbstraction.Bussines
